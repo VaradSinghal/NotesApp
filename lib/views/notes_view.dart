d23092b85b209ccbafc1bb0030c5c89ebf177083
VaradSinghal <varadsinghal1526@gmail.com>
2025-03-11 15:39:00 +0530

    added crud services

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:myapp/constants/routes.dart';
 import 'package:myapp/enums/menu_action.dart';
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/constants/routes.dart';
@@ -28,7 +30,7 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       });
 
       if (isEmailVerified) {
-        // Navigate to NotesView if email is verified
+
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => const NotesView()),
         );
