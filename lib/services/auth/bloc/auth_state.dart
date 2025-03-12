53beecc89e313279714582c4a4da0941eb6cec87
VaradSinghal <varadsinghal1526@gmail.com>
2025-03-13 00:42:51 +0530

    added bloc

---
+++ lib/services/auth/bloc/auth_state.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:myapp/services/auth/auth_user.dart';
+import 'package:equatable/equatable.dart';
+
+@immutable
+abstract class AuthState {
+  final bool isLoading;
+  final String? loadingText;
+  const AuthState({
+    required this.isLoading,
+    this.loadingText = 'Please wait a moment',
+  });
+}
+
+class AuthStateLoading extends AuthState {
+  const AuthStateLoading();
+}
+
+class AuthStateLoggedIn extends AuthState {
+  final AuthUser user;
+  const AuthStateLoggedIn({required this.user, required bool isLoading})
+    : super(isLoading: isLoading);
+}
+
+class AuthStateLoginFailure extends AuthState {
+  final Exception exception;
+ const AuthStateLoginFailure(this.exception);
+}
+
+
+class AuthStateNeedsVerification extends AuthState {
+  const AuthStateNeedsVerification({required bool isLoading})
+    : super(isLoading: isLoading);
+}
+
+class AuthStateLoggedOut extends AuthState {
+  final Exception? exception;
+  const AuthStateLoggedOut({
+    required this.exception,
+    required bool isLoading,
+    String? loadingText,
+  }) : super(isLoading: isLoading, loadingText: loadingText);
+
+  @override
+  List<Object?> get props => [exception, isLoading];
+}
+
+class AuthStateLogOutFailure extends AuthState {
+  final Exception exception;
+ const AuthStateLogOutFailure(this.exception);
+}
+
+
+
+
+
