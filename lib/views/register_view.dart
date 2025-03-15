b9f46015e98b9e7e6e7be8680ec4efb14d82ccd7
VaradSinghal <varadsinghal1526@gmail.com>
2025-03-15 12:36:19 +0530

    fixed scroll issue

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -52,49 +52,51 @@ class _RegisterViewState extends State<RegisterView> {
         appBar: AppBar(title: const Text('Register')),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('Enter your email and password'),
-              TextField(
-                controller: _email,
-                enableSuggestions: false,
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                autofocus: true,
-                decoration: InputDecoration(hintText: 'Enter your email here'),
-              ),
-              TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(hintText: 'Enter your password here'),
-              ),
-              Column(
-                children: [
-                  TextButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      context.read<AuthBloc>().add(
-                        AuthEventRegister(email, password),
-                      );
-                    },
-                            
-                    child: const Text('Register'),
-                  ),
-                  TextButton(
-                onPressed: () {
-                  context.read<AuthBloc>().add(const AuthEventLogOut());
-                },
-                child: const Text('Already registered? Login here!'),
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Enter your email and password'),
+                TextField(
+                  controller: _email,
+                  enableSuggestions: false,
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  autofocus: true,
+                  decoration: InputDecoration(hintText: 'Enter your email here'),
+                ),
+                TextField(
+                  controller: _password,
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: InputDecoration(hintText: 'Enter your password here'),
+                ),
+                Column(
+                  children: [
+                    TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        context.read<AuthBloc>().add(
+                          AuthEventRegister(email, password),
+                        );
+                      },
+                              
+                      child: const Text('Register'),
+                    ),
+                    TextButton(
+                  onPressed: () {
+                    context.read<AuthBloc>().add(const AuthEventLogOut());
+                  },
+                  child: const Text('Already registered? Login here!'),
+                ),
+              ],
+                  
+                ),
                 
-              ),
-              
-            ],
+              ],
+            ),
           ),
         ),
       ),
